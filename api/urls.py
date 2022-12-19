3a831fea110819c04137ef8d751bd4c8796c1046
Piyush Mishra <piyushrmishra143@gmail.com>
2022-12-19 18:53:02 +0530

    djoser uninstall

--- api/urls.py
+++ api/urls.py
@@ -12,17 +12,20 @@ router.register('exhibitors', views.ExhibitorViewSet)
 router.register('visitors', views.VisitorViewSet)
 router.register('visitor/register',views.VisitorCreate)
 router.register('visitor/profile',views.VisitorProfileViewSet)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-pprint.pprint(router.urls)
+# pprint.pprint(router.urls)
 
 urlpatterns = [
     path('', views.index, name='api_home'),
     # path('visitors/', views.VisitorList.as_view()),
     path('exhibitor/',include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  
     path('',include(router.urls)),
-    
-    
 
 ]
